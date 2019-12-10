02e6e09851747161f18c76de28f345ad3bae60a4
Anim-101 <anmaksh@gmail.com>
2019-12-10 10:34:21 +0600

    Added Data Print First Recursive Display Linked List in Linked List of DsA

---
+++ DsA/Data Structures/Linked Lists/Recursive Display Data First Linked List.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+*firstNode = NULL;
+
+void createNodes(int array[], int numberOfNodes)
+{
+    struct Node *tempNode, *lastNode;
+
+    firstNode = (struct Node *) malloc(sizeof(struct Node));
+
+    firstNode->data = array[0];
+
+    firstNode->next = NULL;
+
+    lastNode = firstNode;
+
+    for(int i = 1; i < numberOfNodes; i++)
+    {
+        tempNode = (struct Node *) malloc(sizeof(struct Node));
+
+        tempNode->data = array[i];
+
+        tempNode->next = NULL;
+
+        lastNode->next = tempNode;
+
+        lastNode = tempNode;
+    }
+}
+
+void recursiveDisplayDataFirstLinkedList(struct Node *pointNode)
+{
+   if(pointNode != NULL)
+   {
+       printf("%d ", pointNode->data);
+
+       recursiveDisplayDataFirstLinkedList(pointNode->next);
+   }
+}
+
+int main()
+{
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    createNodes(array, 10);
+
+    recursiveDisplayDataFirstLinkedList(firstNode);
+
+    return 0;
+}
