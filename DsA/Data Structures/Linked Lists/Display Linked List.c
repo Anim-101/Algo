8347f4a6ec6f58f44c00c5d42417cf2c15e159bc
Anim-101 <anmaksh@gmail.com>
2019-12-10 10:16:44 +0600

    Adding DisplayLinkedList.c in Linked Lists of DsA

---
+++ DsA/Data Structures/Linked Lists/Display Linked List.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*firstNode = NULL;
+
+void createNodes(int array[], int numberOfNodes)
+{
+    struct Node *tempNode, *lastNode;
+
+    firstNode = (struct Node *) malloc(sizeof(struct Node));
+
+    firstNode->data = array[0];
+
+    firstNode->next = NULL;
+
+    lastNode = firstNode;
+
+    for(int i = 1; i < numberOfNodes; i++)
+    {
+        tempNode = (struct Node *) malloc(sizeof(struct Node));
+
+        tempNode->data = array[i];
+
+        tempNode->next = NULL;
+
+        lastNode->next = tempNode;
+
+        lastNode = tempNode;
+    }
+}
+
+void displayNodes(struct Node *pointNode)
+{
+    while(pointNode != NULL)
+    {
+        printf("%d ", pointNode->data);
+
+        pointNode = pointNode->next;
+    }
+}
+
+int main()
+{
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    createNodes(array, 10);
+
+    displayNodes(firstNode);
+
+    return 0;
+}
